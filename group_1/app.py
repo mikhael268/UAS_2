f1099eb7a7b62aeb683c61cfca25e0d0b2850964
mikhael268 <mikhaelirawan3@gmail.com>
2025-06-04 20:57:44 +0700

    Login udah kelar, tapi tampilannya belom

--- group_1/app.py
+++ group_1/app.py
@@ -22,6 +22,42 @@ def home():
         return render_template('halaman_depan.html', username=session['username'])
     else:
         return render_template('halaman_depan.html')
+    
+@app.route('/login', methods=['GET', 'POST'] )
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        pwd = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute(f"select username, password from tbl_user where username = '{username}'")
+        user = cur.fetchone()
+        if user and pwd == user[1]:
+            session['username'] = user[0]
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    else:
+        return render_template('login.html')
+    
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        cur = mysql.connection.cursor()
+        cur.execute(f"INSERT INTO tbl_user (username, password) VALUES ('{username}', '{password}')")
+        mysql.connection.commit()
+        cur.close()
+        
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
